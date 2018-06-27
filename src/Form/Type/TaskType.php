a54b0b9d48822e77e95beaa0abca981a95bf5de8
ulrich <ulm_ulm@hotmail.com>
2018-06-27 13:48:02 +0200

    label in TaskType added

--- src/Form/Type/TaskType.php
+++ src/Form/Type/TaskType.php
@@ -16,6 +16,7 @@ use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -30,8 +31,8 @@ class TaskType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content', TextareaType::class)
+            ->add('title', TextType::class, ['label' => 'Titre'])
+            ->add('content', TextareaType::class, ['label' => 'Contenu'])
         ;
     }
 
